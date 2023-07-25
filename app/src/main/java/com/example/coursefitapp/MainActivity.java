ad3ef2e40f174e1482e80c33978dcc72bc8b6a0d
Ahmad Syahmi <ahmad.syahmi3211@gmail.com>
2023-07-25 15:36:46 +0800

    login and signup button functionality added

--- app/src/main/java/com/example/coursefitapp/MainActivity.java
+++ app/src/main/java/com/example/coursefitapp/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.coursefitapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public Button signupButton;
+    public Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String welcomeMessage = getString(R.string.welcome_message);
+        TextView textView = findViewById(R.id.welcomeText);
+        textView.setText(welcomeMessage);
+
+        signupButton = findViewById(R.id.signupButton);
+        loginButton = findViewById(R.id.loginButton);
+
+        //signup button
+        signupButton.setOnClickListener(v -> {
+            // This method will be called when the button is clicked
+            Intent intent = new Intent(MainActivity.this, SignupPage.class);
+            startActivity(intent);
+        });
+
+        //login button
+        loginButton.setOnClickListener(v -> {
+            // This method will be called when the button is clicked
+            Intent intent = new Intent(MainActivity.this, LoginPage.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/coursefitapp/SignupPage.java
@@ -0,0 +1,15 @@
+package com.example.coursefitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class SignupPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_page);
+    }
+}
