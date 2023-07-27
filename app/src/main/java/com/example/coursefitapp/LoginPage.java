f74b68012ef271d96896cb679e3f6b5664b3571b
Ahmad Syahmi <ahmad.syahmi3211@gmail.com>
2023-07-27 16:32:45 +0800

    login and signup UI created main menu UI created

--- app/src/main/java/com/example/coursefitapp/LoginPage.java
+++ app/src/main/java/com/example/coursefitapp/LoginPage.java
@@ -2,13 +2,24 @@ package com.example.coursefitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
 
+    public Button nextButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        nextButton = findViewById(R.id.nextButton);
+
+        nextButton.setOnClickListener(v -> {
+            // This method will be called when the button is clicked
+            Intent intent = new Intent(LoginPage.this, MainMenu.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/coursefitapp/SignupPage.java
+++ app/src/main/java/com/example/coursefitapp/SignupPage.java
@@ -2,14 +2,24 @@ package com.example.coursefitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 
 public class SignupPage extends AppCompatActivity {
-
+    public Button nextButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_page);
+
+        nextButton = findViewById(R.id.nextButton);
+
+        nextButton.setOnClickListener(v -> {
+            // This method will be called when the button is clicked
+            Intent intent = new Intent(SignupPage.this, MainMenu.class);
+            startActivity(intent);
+        });
     }
 }
