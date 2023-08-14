8160d0ec8cf8717134b150081a910f71c0161e1f
Ahmad Syahmi <ahmad.syahmi3211@gmail.com>
2023-08-14 16:00:49 +0800

    - Main menu updated with sign out

--- app/src/main/java/com/example/coursefitapp/MainMenu.java
+++ app/src/main/java/com/example/coursefitapp/MainMenu.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
@@ -22,5 +24,21 @@ public class MainMenu extends AppCompatActivity {
             Intent intent = new Intent(MainMenu.this, bahagianA.class);
             startActivity(intent);
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_sign_out) {
+            // Perform sign-out logic here
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
